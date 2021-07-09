822786ddf222091d2cb9cab81aca699fb0a73742
sruthi <35577802+sruthilay@users.noreply.github.com>
2021-07-08 19:34:20 -0700

    Create webserver.tf

---
+++ webserver.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+#create security group ,allow ssh and http
+
+resource "aws_security_group" "webserver_access" {
+        name = "webserver_access"
+        description = "allow ssh and http"
+
+ // Outgoing
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["55.55.55.55/24"]
+  }
+
+  // Incoming
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["55.55.55.55/24"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["55.55.55.55/24"]
+  }
+#security group end here
+
+#creating aws ec2 instance 
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0b28dfc7adc325ef4"
+  availability_zone = "us-west-2"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.webserver_access.name}"]
+  key_name = "webserver"
+  user_data = <<-EOF
+        #!/bin/bash -xe
+        sudo yum install httpd -y
+        sudo systemctl start httpd
+        sudo systemctl enable httpd
+        echo "<h1>sample webserver using terraform</h1>" | sudo tee /var/www/html/index.html
+  EOF
+
+  tags = {
+    Name  = "webserver"
+    Stage = "testing"
+    Location = "USA"
+  }
+}
+}
